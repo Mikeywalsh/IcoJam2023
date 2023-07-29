c68c8f5cbf141efa4814d9a5f0aeb0fa8908df72
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2023-07-29 17:59:16 +0100

    Basic player animation

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    private static readonly int IsRunningHash = Animator.StringToHash("IsRunning");
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    public void SetIsRunning(bool value)
+    {
+        _animator.SetBool(IsRunningHash, value);
+    }
+}
