f1c13a1590a41857195ff08c3b14bbdfd2481172
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2023-07-30 15:16:29 +0100

    Portal effect bug fixes

--- Assets/Scripts/PastPlayerResetEffect.cs
+++ Assets/Scripts/PastPlayerResetEffect.cs
@@ -1,59 +1,42 @@
 ﻿using DG.Tweening;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class PastPlayerResetEffect : MonoBehaviour
 {
-    public class PastPlayerResetEffect : MonoBehaviour
+    [SerializeField] private float _duration = 0.6f;
+
+    [SerializeField] private Vector3 _portalMaxScale = new Vector3(2f, 3.2f, 2f);
+
+    [SerializeField] private Transform _portal;
+
+    [SerializeField] private Transform _playerModel;
+
+    private Tween _portalTween;
+    private Tween _playerModelTween;
+
+    private void Start()
     {
-        [SerializeField]
-        private float _duration = 0.6f;
-        
-        [SerializeField]
-        private Vector3 _portalMaxScale = new Vector3(2f, 3.2f, 2f);
-        
-        [SerializeField]
-        private Transform _portal;
+        _portal.localScale = Vector3.zero;
+        _portalTween = _portal.DOScale(_portalMaxScale, _duration / 3)
+            .SetEase(Ease.InOutBack)
+            .OnComplete(ShrinkPortalAndPlayer);
+    }
 
-        [SerializeField]
-        private Transform _playerModel;
-        
-        private Tween _portalTween;
-        private Tween _playerModelTween;
+    private void ShrinkPortalAndPlayer()
+    {
+        _portalTween = _portal.DOScale(Vector3.zero, _duration * 2 / 3)
+            .SetEase(Ease.InOutBack);
 
-        private void Start()
-        {
-            RotatePortalToPointUp();
-            _portal.localScale = Vector3.zero;
-            _portalTween = _portal.DOScale(_portalMaxScale, _duration * 1 / 3)
-                .SetEase(Ease.InOutBack)
-                .OnComplete(ShrinkPortalAndPlayer);
-        }
+        _playerModelTween = _playerModel.DOScale(Vector3.zero, _duration * 2 / 3)
+            .SetEase(Ease.InOutBack)
+            .OnComplete(OnEffectComplete);
+    }
 
-        private void RotatePortalToPointUp()
-        {
-            var worldUpDirection = Vector3.up;
-            var targetRotation = Quaternion.FromToRotation(transform.up, worldUpDirection) * transform.rotation;
+    private void OnEffectComplete()
+    {
+        _portalTween.Kill();
+        _playerModelTween.Kill();
 
-            _portal.rotation = targetRotation;
-        }
-        
-
-        private void ShrinkPortalAndPlayer()
-        {
-            _portalTween = _portal.DOScale(Vector3.zero, _duration * 2 / 3)
-                .SetEase(Ease.InOutBack); 
-
-            _playerModelTween = _playerModel.DOScale(Vector3.zero, _duration * 2 / 3)
-                .SetEase(Ease.InOutBack)
-                .OnComplete(OnEffectComplete);
-        }
-
-        private void OnEffectComplete()
-        {
-            _portalTween.Kill();
-            _playerModelTween.Kill();
-            
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
