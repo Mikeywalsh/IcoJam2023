eda68c171ee639cbc0573cbb2d73582cd97f6280
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2023-07-30 16:53:24 +0100

    Proper Doors!

--- Assets/Scripts/Traps/DoorTemporal.cs
+++ Assets/Scripts/Traps/DoorTemporal.cs
@@ -5,8 +5,12 @@ namespace Traps
 {
     public class DoorTemporal : DependantBoolTemporal
     {
-        private Tween _rightTween;
-        private Tween _leftTween;
+        private Tween _rightDoorPositionTween;
+        private Tween _leftDoorPositionTween;
+        private Tween _rightDoorScaleTween;
+        private Tween _leftDoorScaleTween;
+        private Tween _rightDoorMiddleTween;
+        private Tween _leftDoorMiddleTween;
         private bool _open;
 
         public override int ExecutionOrder() => 1;
@@ -31,11 +35,29 @@ namespace Traps
             }
 
             _open = true;
-            _rightTween?.Kill();
-            _rightTween = transform.GetChild(0).DOLocalMoveX(1.5f, 0.2f)
+            
+            // Door move
+            _rightDoorPositionTween?.Kill();
+            _rightDoorPositionTween = transform.GetChild(0).DOLocalMoveX(1f, 0.2f)
                 .SetEase(Ease.Linear);
-            _leftTween?.Kill();
-            _leftTween = transform.GetChild(1).DOLocalMoveX(-1.5f, 0.2f)
+            _leftDoorPositionTween?.Kill();
+            _leftDoorPositionTween = transform.GetChild(1).DOLocalMoveX(-1f, 0.2f)
+                .SetEase(Ease.Linear);
+            
+            // Door scale
+            _rightDoorScaleTween?.Kill();
+            _rightDoorScaleTween = transform.GetChild(0).DOScaleY(0f, 0.2f)
+                .SetEase(Ease.Linear);
+            _leftDoorScaleTween?.Kill();
+            _leftDoorScaleTween = transform.GetChild(1).DOScaleY(0f, 0.2f)
+                .SetEase(Ease.Linear);
+            
+            // Door middle
+            _rightDoorMiddleTween?.Kill();
+            _rightDoorMiddleTween = transform.GetChild(2).DOLocalMoveX(.95f, 0.2f)
+                .SetEase(Ease.Linear);
+            _leftDoorMiddleTween?.Kill();
+            _leftDoorMiddleTween = transform.GetChild(3).DOLocalMoveX(-.95f, 0.2f)
                 .SetEase(Ease.Linear);
         }
         
@@ -47,12 +69,32 @@ namespace Traps
             }
 
             _open = false;
-            _rightTween?.Kill();
-            _rightTween = transform.GetChild(0).DOLocalMoveX(0.5f, 0.2f)
+            
+            // Door move
+            _rightDoorPositionTween?.Kill();
+            _rightDoorPositionTween = transform.GetChild(0).DOLocalMoveX(0f, 0.2f)
                 .SetEase(Ease.Linear);
-            _leftTween?.Kill();
-            _leftTween = transform.GetChild(1).DOLocalMoveX(-0.5f, 0.2f)
+            _leftDoorPositionTween?.Kill();
+            _leftDoorPositionTween = transform.GetChild(1).DOLocalMoveX(0f, 0.2f)
                 .SetEase(Ease.Linear);
+            
+            
+            // Door scale
+            _rightDoorScaleTween?.Kill();
+            _rightDoorScaleTween = transform.GetChild(0).DOScaleY(100, 0.2f)
+                .SetEase(Ease.Linear);
+            _leftDoorScaleTween?.Kill();
+            _leftDoorScaleTween = transform.GetChild(1).DOScaleY(100, 0.2f)
+                .SetEase(Ease.Linear);
+            
+            // Door middle
+            _rightDoorMiddleTween?.Kill();
+            _rightDoorMiddleTween = transform.GetChild(2).DOLocalMoveX(0f, 0.2f)
+                .SetEase(Ease.Linear);
+            _leftDoorMiddleTween?.Kill();
+            _leftDoorMiddleTween = transform.GetChild(3).DOLocalMoveX(0f, 0.2f)
+                .SetEase(Ease.Linear);
+            
         }
     }
 }
